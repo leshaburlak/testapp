d4bc87f3e4c5ef54de6ed36c9b8f063a9250d310
leshaburlak <1moreasdf@gmail.com>
2021-11-23 16:36:48 +0300

    init

---
+++ test/arrival_assignment/core_test.clj
@@ -0,0 +1,7 @@
+(ns arrival-assignment.core-test
+  (:require [clojure.test :refer :all]
+            [arrival-assignment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
